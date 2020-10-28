159ac3e8dd517fbaab681413ce26f39a83f92364
Gunnar Oledal <gunnar.oledal@gmail.com>
2020-10-28 13:56:13 +0100

    Quick test to run signaing on ip

--- app/src/boot/socket-io.js
+++ app/src/boot/socket-io.js
@@ -1,7 +1,8 @@
 import io from 'socket.io-client';
 import VueSocketIOExt from 'vue-socket.io-extended';
 
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
+const socket = io('http://192.168.1.207:3000');
 
 export default async ({ app, router, store, Vue }) => {
   Vue.use(VueSocketIOExt, socket, { store });
