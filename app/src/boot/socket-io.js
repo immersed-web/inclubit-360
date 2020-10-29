92f732a732f9ea011adfeccc41b3d0ee2c94772c
Gunnar Oledal <gunnar.oledal@gmail.com>
2020-10-29 13:15:30 +0100

    Fixing undefined variable bug

--- app/src/boot/socket-io.js
+++ app/src/boot/socket-io.js
@@ -2,10 +2,9 @@ import io from 'socket.io-client';
 import VueSocketIOExt from 'vue-socket.io-extended';
 
 // const socket = io('http://localhost:3000');
+let socketUrl = `${process.env.BACKEND_SERVER_PROTOCOL}://${process.env.BACKEND_SERVER}/`;
 if (process.env.SIGNALING_PORT) {
   socketUrl = `${process.env.BACKEND_SERVER_PROTOCOL}://${process.env.BACKEND_SERVER}:${process.env.SIGNALING_PORT}/`
-} else {
-  socketUrl = `${process.env.BACKEND_SERVER_PROTOCOL}://${process.env.BACKEND_SERVER}/`
 }
 
 const socket = io(socketUrl);
