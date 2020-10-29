20c859435c0b4237286cc09620683e794efa8950
Gunnar Oledal <gunnar.oledal@gmail.com>
2020-10-29 16:40:10 +0100

    adding dotenv app extension so we can inject variables with .env file

--- app/src/boot/socket-io.js
+++ app/src/boot/socket-io.js
@@ -1,11 +1,14 @@
 import io from 'socket.io-client';
 import VueSocketIOExt from 'vue-socket.io-extended';
 
-// const socket = io('http://localhost:3000');
+// const socketUrl = 'http://localhost:3000';
+
 let socketUrl = `${process.env.BACKEND_SERVER_PROTOCOL}://${process.env.BACKEND_SERVER}/`;
 if (process.env.SIGNALING_PORT) {
   socketUrl = `${process.env.BACKEND_SERVER_PROTOCOL}://${process.env.BACKEND_SERVER}:${process.env.SIGNALING_PORT}/`
 }
+
+console.log('socketUrl: ', socketUrl);
 
 const socket = io(socketUrl);
 
