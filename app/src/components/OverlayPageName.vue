0e0cb828c29f8ce3d1d73bbfa3e796f820bd2182
Gunnar Oledal <gunnar.oledal@gmail.com>
2020-11-27 19:35:45 +0100

    Renamed and repurposed overlay component

---
+++ app/src/components/OverlayPageName.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="container">
+    <h4 class="overlay-heading">
+      {{ text }}
+    </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OverlayPageName',
+  props: {
+    // text: {
+    //   required: true,
+    //   type: String,
+    // },
+  },
+  data () {
+    return {
+      // text: '',
+    };
+  },
+  computed: {
+    text () {
+      if (this.$route.name) {
+        return this.$route.name;
+      } else {
+        return this.$route.fullPath.replace(/^\/+/g, '');
+      }
+    },
+  },
+  created () {
+
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#container {
+  position: fixed;
+  left: 2rem;
+  bottom: 2rem;
+  pointer-events: none;
+}
+.overlay-heading {
+  margin: 0;
+  color: rgba(255, 255, 255, 0.6);
+
+}
+</style>
