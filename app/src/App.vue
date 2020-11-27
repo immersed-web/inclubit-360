77c4db6a716552f5b7c046b9982c5c3aa1cbb684
Gunnar Oledal <gunnar.oledal@gmail.com>
2020-11-27 19:36:02 +0100

    retore some settings from storage on refresh

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   name: 'App',
+  created () {
+    this.restoreConnectionSettings();
+  },
+  methods: {
+    ...mapActions('connectionSettings', {
+      restoreConnectionSettings: 'setSettingsFromStorage',
+    }),
+  },
 };
 </script>
